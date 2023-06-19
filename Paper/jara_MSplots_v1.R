ed5d2cdd7f5a52204bd390570ddb232234873a1f
Richard Sherley <r.sherley@exeter.ac.uk>
2023-06-19 19:04:44 +0100

    Longer runs, updated plots with new R package functionality, added supplementary plots, added PDF outputs

--- Paper/jara_MSplots_v1.R
+++ Paper/jara_MSplots_v1.R
@@ -1,23 +1,30 @@
 #><>><>><>><>><>><>><>><>><>><>><>><>><>><>><>
 # library(JARA)
-# Code to produce figures for JARA Paper 
+# Code to produce figures for Winker, Pacoureau & Sherley: 
+# JARA: An R package to facilitate IUCN Red Listing using population declines
 #
 # github.com/henning-winker/JARA 
 #
 # Henning Winker (henning.winker@gmail.com) 
-# Joint Research Centre of the European Commission
-# Work Email: Henning.Winker@ec.europa.eu 
+# Richard Sherley (r.sherley@exeter.ac.uk) 
+# Nathan Pacoureau (Pacoureau Nathan <n.pacoureau@gmail.com>)
 #><>><>><>><>><>><>><>><>><>><>><>><>><>><>><>
 
-setwd("C:/Work/Research/MS_JARA2020/MSplots")
 # Install JARA
-#devtools::install_github("henning-winker/jara")
+devtools::install_github("henning-winker/jara",force=T)
 
 # load pakage
 library(JARA)
 # load data
 # jara.examples # inputs
 # jrdat # Indices
+
+# JAGS settings
+ni<-25000 
+nb<-5000
+nt<-5
+nc<-3
+
 
 specs = data.frame(Species=jara.examples$assessment)
 specs
@@ -27,12 +34,12 @@ sp = jara.examples$assessment[i]
 pars =  jara.examples[i,]
 dat = jrdat$Yellowspot_skate
 # Build JARA input 
-jr1 = build_jara(I=dat$I,se=dat$SE,
+jr2 = build_jara(I=dat$I,se=dat$SE,
                  assessment=sp,
                  scenario = "MS",
                  GL=pars$generation.length)
 # Fit JARA
-fit1 = fit_jara(jr1,save.jara = T)   
+fit2 = fit_jara(jr2,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 
 # Run Whitespotted Smoothhound 
 i = 7
@@ -40,12 +47,12 @@ sp = jara.examples$assessment[i]
 pars =  jara.examples[i,]
 dat = jrdat$SmoothhoundShark
 # Build JARA input 
-jr2 = build_jara(I=dat$I,se=dat$SE,
+jr1 = build_jara(I=dat$I,se=dat$SE,
                  assessment=sp,
                  scenario = "MS",
                  GL=pars$generation.length)
 # Fit JARA
-fit2 = fit_jara(jr2,save.jara = T)   
+fit1 = fit_jara(jr1,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 
 #*************************
 # Figure 1: Indices
@@ -53,33 +60,42 @@ fit2 = fit_jara(jr2,save.jara = T)
 l = 1.2
 plname = "Fig1"
 pwidth = 8
-pheight = 7
+pheight = 10.5
+panels = 6
 res=300
 adj=-.15
-windows(width=pwidth,height=pheight) # Sorry Rich
-jrpar(mfrow=c(2,2),labs=T,plot.cex=1)
+
+quartz(width=pwidth,height=pheight)  # use windows for PC, quartz for Mac
+jrpar(mfrow=c((panels/2),2),labs=T,plot.cex=1)
 jrplot_indices(jr1,add=T)
 mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
 jrplot_indices(jr2,add=T)
 mtext(text=paste0(letters[2],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_trjfit(fit1,add=T)
+jrplot_trjfit(fit1,add=T,legend.pos = "bottomleft")
 mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_trjfit(fit2,add=T)
+jrplot_trjfit(fit2,add=T,legend.pos = "bottomleft")
 mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
-mtext(c("Yellowspotted Skate","Whitespotted Houndshark"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
+jrplot_residuals(fit1,add=T,legend.pos = "bottomleft")
+mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_residuals(fit2,add=T,legend.pos = "bottomleft")
+mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
+mtext(c("Whitespot Smoothhound","Yellowspotted Skate"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
 
 dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
 dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
 
 #*************************
 # Figure 2: Decision plot
 #*************************
 plname = "Fig2"
 pwidth = 8
-pheight = 8.5
+pheight = 10.5
 res=300
-windows(width=pwidth,height=pheight) # Sorry Rich
-jrpar(mfrow=c(3,2),labs=T,plot.cex=1)
+panels = 6
+quartz(width=pwidth,height=pheight) # use windows for PC, quartz for Mac
+jrpar(mfrow=c((panels/2),2),labs=T,plot.cex=1)
 jrplot_poptrj(fit1,add=T)
 mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
 jrplot_poptrj(fit2,add=T)
@@ -88,59 +104,20 @@ jrplot_changes(fit1,add=T)
 mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
 jrplot_changes(fit2,add=T)
 mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_iucn(fit1,add=T)
+jrplot_iucn(fit1,NT=FALSE,add=T,legend.cex = 0.8)
 mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_iucn(fit2,add=T)
+jrplot_iucn(fit2,NT=FALSE,add=T,legend.cex = 0.8)
 mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
-mtext(c("Yellowspotted Skate","Whitespotted Houndshark"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
+mtext(c("Whitespot Smoothhound","Yellowspotted Skate"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
 
 dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
 dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
-
-#--------------------------
-# Do hindcasting
-#-------------------------
-runhc = FALSE # need to run one time
-if(runhc==T){
-hc1 = jara_hindcast(jr1,speedup = F,save.jarafile = F)
-hc2 = jara_hindcast(jr2,speedup = F,save.jarafile = F)
-save(hc1,file="hc1.rdata")
-save(hc2,file="hc2.rdata")} else{
-  load("hc1.rdata",verbose=T)
-  load("hc2.rdata",verbose=T)
-}
-
-
-#*************************
-# Figure 3: Retrospectives
-#*************************
-plname = "Fig3"
-pwidth = 8
-pheight = 8.5
-res=300
-adj=-.15
-windows(width=pwidth,height=pheight) # Sorry Rich
-jrpar(mfrow=c(3,2),labs=T,plot.cex=1)
-jrplot_state(fit1,add=T)
-mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_state(fit2,add=T)
-mtext(text=paste0(letters[2],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_retrobias(hc1,add=T)
-mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_retrobias(hc2,add=T,legend.loc = "topleft")
-mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_retroiucn(hc1,add=T,add.legend = F)
-mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_retroiucn(hc2,add=T,add.legend = F)
-mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
-mtext(c("Yellowspotted Skate","Whitespotted Houndshark"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
-
-dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
-dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
 
 
 #******************************************
-# Figure 4: Blue Marlin CPUE vs Assessment
+# Figure 3: Blue Marlin CPUE vs Assessment
 #******************************************
 specs
 # Run CPUEs 
@@ -154,7 +131,7 @@ jr3 = build_jara(I=dat$I,se=dat$SE,
                  scenario = "MS",
                  GL=pars$generation.length)
 # Fit JARA
-fit3 = fit_jara(jr3,save.jara = T)   
+fit3 = fit_jara(jr3,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 
 # Run Assessment output 
 i = 6
@@ -167,15 +144,16 @@ jr4 = build_jara(I=dat$I,se=dat$SE,
                  scenario = "MS",
                  GL=pars$generation.length)
 # Fit JARA
-fit4= fit_jara(jr4,save.jara = T)   
+fit4= fit_jara(jr4,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 
 # Make Figure
-plname = "Fig4"
+plname = "Fig3"
 pwidth = 8
-pheight = 8.5
+pheight = 10.5
+panels = 6
 res=300
-windows(width=pwidth,height=pheight) # Sorry Rich
-jrpar(mfrow=c(3,2),labs=T,plot.cex=1)
+quartz(width=pwidth,height=pheight) # use windows for PC, quartz for Mac
+jrpar(mfrow=c((panels/2),2),labs=T,plot.cex=1)
 jrplot_trjfit(fit3,add=T)
 mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
 jrplot_trjfit(fit4,add=T)
@@ -184,17 +162,19 @@ jrplot_poptrj(fit3,add=T)
 mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
 jrplot_poptrj(fit4,add=T)
 mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_iucn(fit3,add=T)
+jrplot_iucn(fit3,NT=FALSE,add=T,legend.cex = 0.8)
 mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_iucn(fit4,add=T)
+jrplot_iucn(fit4,NT=FALSE,add=T,legend.cex = 0.8)
 mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
 mtext(c("CPUE Indices","Stock Assessment"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
 
 dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
 dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
 
 #******************************************
-# Figure 5: Cape Gannet
+# Figure 4: Cape Gannet
 #******************************************
 specs
 # Run CPUEs 
@@ -207,23 +187,24 @@ jr5 = build_jara(I=dat$I,se=dat$SE,
                  assessment=sp,
                  model.type = "census",
                  scenario = "MS",
-                 GL=pars$generation.length)
+                 GL=18.3)
+#GL=pars$generation.length)
 # Fit JARA
-fit5 = fit_jara(jr5,save.jara = T)   
+fit5 = fit_jara(jr5,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 
 # do user projections
 jr5p = build_jara(I=dat$I,se=dat$SE,
-                 assessment=sp,
-                 model.type = "census",
-                 scenario = "MS",proj.yrs.user = 20,
-                 pk.prior = c(0.8,0.1))
+                  assessment=sp,
+                  model.type = "census",
+                  scenario = "MS",proj.yrs.user = 18,
+                  pk.prior = c(0.8,0.1))
 # Assumes that those colonies that currently increasing have 
 # a maximum growth potential + 20% relative their historical maximum
 # i.e. Algoa Bay and Lamberts Bay and Malgas
 fit5$pars
 # Interesting story line
 # Fit JARA
-fit5p = fit_jara(jr5p,save.jara = T)   
+fit5p = fit_jara(jr5p,save.jara = T,ni=ni, nb=nb, nt=nt, nc=nc)   
 # check
 jrplot_poptrj(fit5)
 jrplot_poptrj(fit5p)
@@ -233,32 +214,105 @@ runhc = FALSE # need to run one time
 if(runhc==T){
   hc5 = jara_hindcast(jr5,peels = 0:10,speedup = F,save.jarafile = F)
   save(hc5,file="hc5.rdata")
-  } else{
-    load("hc5.rdata",verbose=T)
-  }
+} else{
+  load("hc5.rdata",verbose=T)
+}
 
 # Make Figure
-plname = "Fig5"
+plname = "Fig4"
 pwidth = 8
-pheight = 8.5
+pheight = 10.5
+panels=6
+adj=-.15
 res=300
-windows(width=pwidth,height=pheight) # Sorry Rich
-jrpar(mfrow=c(3,2),labs=T,plot.cex=1)
-jrplot_trjfit(fit5,add=T)
+quartz(width=pwidth,height=pheight)  # use windows for PC, quartz for Mac
+jrpar(mfrow=c((panels/2),2),labs=T,plot.cex=1)
+jrplot_trjfit(fit5,add=T,ylab="Breeding pairs x 1000")
 mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_changes(fit5,add=T)
+jrplot_poptrj(fit5,add=T,ylab="Breeding pairs x 1000")
 mtext(text=paste0(letters[2],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_poptrj(fit5,add=T)
+jrplot_iucn(fit5,add=T,NT=FALSE)
 mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_iucn(fit5,add=T)
+jrplot_changes(fit5,add=T)
 mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_poptrj(fit5p,add=T)
+jrplot_retroiucn(hc5,add=T, add.legend = F)
 mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
-jrplot_retroiucn(hc5,add=T,add.legend = F)
+jrplot_state(fit5p,add=T,ref.yr=c(1956:1958))
 mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
-mtext(c("CPUE Indices","Stock Assessment"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
 
 dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
 dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
 
-# End (for now)
+
+#******************************************
+# Figure S5: SA Elasmobranchs Retrospectives
+#******************************************
+
+#--------------------------
+# Do hindcasting
+#-------------------------
+runhc = F # need to run one time
+if(runhc==T){
+  hc1 = jara_hindcast(jr1,speedup = F,save.jarafile = F)
+  hc2 = jara_hindcast(jr2,speedup = F,save.jarafile = F)
+  save(hc1,file="hc1.rdata")
+  save(hc2,file="hc2.rdata")} else{
+    load("hc1.rdata",verbose=T)
+    load("hc2.rdata",verbose=T)
+  }
+
+plname = "FigS5"
+pwidth = 8
+pheight = 8.5
+res=300
+adj=-.15
+quartz(width=pwidth,height=pheight) # use windows for PC, quartz for Mac
+jrpar(mfrow=c(3,2),labs=T,plot.cex=1)
+jrplot_state(fit1,add=T)
+mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_state(fit2,add=T)
+mtext(text=paste0(letters[2],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_retrobias(hc1,add=T,legend.loc = "topleft")
+mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_retrobias(hc2,add=T,legend.loc = "topright")
+mtext(text=paste0(letters[4],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_retroiucn(hc1,add=T,add.legend = F)
+mtext(text=paste0(letters[5],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_retroiucn(hc2,add=T,add.legend = F)
+mtext(text=paste0(letters[6],")"), xpd=NA, side=3, adj=adj, cex=1)
+mtext(c("Whitespot Smoothhound","Yellowspotted Skate"), side=3, outer=T,line= -0.2,cex=1.,c(0.27,0.77))
+
+dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
+
+
+
+#******************************************
+# Figure S6: Additional Cape Gannet plots
+#******************************************
+
+plname = "FigS6"
+pwidth = 12
+pheight = 4
+res=300
+adj=-.15
+quartz(width=pwidth,height=pheight) # use windows for PC, quartz for Mac
+jrpar(mfrow=c(1,3),labs=T,plot.cex=1)
+jrplot_poptrj(fit5p,add=T,ylab="Breeding pairs x 1000")
+mtext(text=paste0(letters[1],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_iucn(fit5p,add=T,NT=F)
+mtext(text=paste0(letters[2],")"), xpd=NA, side=3, adj=adj, cex=1)
+jrplot_retrobias(hc5,add=T,legend.loc = "topright")
+mtext(text=paste0(letters[3],")"), xpd=NA, side=3, adj=adj, cex=1)
+mtext(c("1G Projection Window"), side=3, outer=T,line= -0.3,cex=1.,c(0.15))
+mtext(c("Observed + Projected (A4)"), side=3, outer=T,line= -0.3,cex=1.,c(0.5))
+mtext(c("Retrospective peels"), side=3, outer=T,line= -0.3,cex=1.,c(0.85))
+
+dev.print(tiff,paste0(getwd(),"/",plname,"_hires.tiff"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(jpeg,paste0(getwd(),"/",plname,".jpg"), width = pwidth, height = pheight, res = res, units = "in")
+dev.print(pdf,paste0(getwd(),"/",plname,".pdf"), width = pwidth, height = pheight)
+dev.off()
